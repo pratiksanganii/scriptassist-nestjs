b0c9b2c632cb098cd828cd4a16f3cdd6d3c76c61
pratiksanganii <pratikpsangani2003@gmail.com>
2025-06-20 15:39:32 +0100

    worker service,   dev script added and used    createApplicationContext   instead NestFactory.create as whole http server not needed

--- src/worker.main.ts
+++ src/worker.main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { TaskProcessorModule } from './queues/task-processor/task-processor.module';
 
 async function bootstrap() {
-  await NestFactory.create(TaskProcessorModule);
+  await NestFactory.createApplicationContext(TaskProcessorModule);
   console.log('Worker is running and listening to task queue...');
 }
 bootstrap();
